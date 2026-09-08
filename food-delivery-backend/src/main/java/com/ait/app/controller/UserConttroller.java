58d096c16ccbeb3bad0846abd58c89ae4b576714
Prasad Jadhav <prasadjadhav779900@gmail.com>
2026-09-08 16:25:19 +0530

    //

--- food-delivery-backend/src/main/java/com/ait/app/controller/UserConttroller.java
+++ food-delivery-backend/src/main/java/com/ait/app/controller/UserConttroller.java
@@ -29,7 +29,7 @@ public class UserConttroller {
 
 		userService.registerUser(user);
 		return new ResponseEntity("User Registerd Successfully", HttpStatus.CREATED);
-
+//
 	}
 
 	@GetMapping("/getUser/{id}")
