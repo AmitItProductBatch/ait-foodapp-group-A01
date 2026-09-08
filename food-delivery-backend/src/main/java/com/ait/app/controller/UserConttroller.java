4941f85490bb4cb4ab2f654d91234a26cce347de
Prasad Jadhav <prasadjadhav779900@gmail.com>
2026-09-08 22:56:07 +0530

    Story 105 is Done

--- food-delivery-backend/src/main/java/com/ait/app/controller/UserConttroller.java
+++ food-delivery-backend/src/main/java/com/ait/app/controller/UserConttroller.java
@@ -3,6 +3,7 @@ package com.ait.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,4 +47,10 @@ public class UserConttroller {
 		return new ResponseEntity(updatedUser, HttpStatus.CREATED);
 
 	}
+	@DeleteMapping("/deleteUser/{id}")
+	public ResponseEntity deleteUser(@PathVariable int id , @RequestBody UsersDto dto ) {
+		userService.deleteUserByID(id,dto);
+		return new ResponseEntity("User Deleted successfully",HttpStatus.CREATED);
+	}
+	
 }
