53b9cf6f2fe62ac92bd1dc546930246a563e0449
Prashik Mohod <prashikmohod45@gmail.com>
2026-09-08 13:00:49 +0530

    Story No.-FOO-103: view profile in that i was writing the code of get user details if user id is not there for that throwing exception and making api for that.

--- food-delivery-backend/src/main/java/com/ait/app/service/UserService.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/UserService.java
@@ -5,9 +5,11 @@ import com.ait.app.dto.UsersDto;
 import com.ait.app.model.Users;
 
 public interface UserService {
-	
-	
-	public UserResponseDto registerUser(UsersDto user);
-	//public void loginUser();
 
+	public UserResponseDto registerUser(UsersDto user);
+
+	// Story no FOO-101 //
+	public UserResponseDto getUserDetails(int id);
+
+	
 }
