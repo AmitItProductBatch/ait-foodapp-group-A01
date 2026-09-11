3e3c45b308e81054e14fb9d4d1aa02a44d835283
Sanskruti-Rajvaidya <sanskrutirajvaidya@gmail.com>
2026-09-11 18:19:27 +0530

    Done

---
+++ food-delivery-backend/src/main/java/com/ait/app/exception/AddressServiceException.java
@@ -0,0 +1,35 @@
+package com.ait.app.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class AddressServiceException extends RuntimeException {
+
+	private String message;
+
+	private HttpStatus httpStatus;
+
+	public AddressServiceException(String message, HttpStatus httpStatus) {
+		super();
+		this.message = message;
+		this.httpStatus = httpStatus;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+
+	public void setHttpStatus(HttpStatus httpStatus) {
+		this.httpStatus = httpStatus;
+	}
+	
+	
+
+}
