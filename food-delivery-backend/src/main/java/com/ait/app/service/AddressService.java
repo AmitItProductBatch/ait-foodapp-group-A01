a806e0f5429c35171d746c9cf33dc97448ffb3e9
anandpatil4502 <metangalea917@gmail.com>
2026-09-12 10:22:02 +0530

    Address update/delete by user id and address id

--- food-delivery-backend/src/main/java/com/ait/app/service/AddressService.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/AddressService.java
@@ -6,4 +6,7 @@ import com.ait.app.dto.AddressDto;
 
 public interface AddressService {
       AddressResponseDto createAddress(int userId, AddressDto addressDto);
+      public AddressResponseDto updateAddress(int userId, int addressId, AddressDto addressDto);
+      public void deleteAddressByA_IdAndU_Id(int userId, int addressId);
+
 }
