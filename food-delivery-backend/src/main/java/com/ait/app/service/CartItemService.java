e72a07c1ad0f2dfbb900a7bcdba8f99255b0e905
Prasad Jadhav <prasadjadhav779900@gmail.com>
2026-09-18 19:19:39 +0530

    Story-172 Add PATCH API to update cart item quantity is Done

--- food-delivery-backend/src/main/java/com/ait/app/service/CartItemService.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/CartItemService.java
@@ -6,9 +6,11 @@ import com.ait.app.dto.CartItemRequest;
 import com.ait.app.dto.CartItemResponse;
 
 public interface CartItemService {
-	
+
 	CartItemResponse addItemToCart(int cartId, CartItemRequest request);
-	
+
 	List<CartItemResponse> getAllCartItemsByCartId(int cartId);
+
+	String updateCartItemQuantity(int cartId, int cartItemId, int quantity);
 
 }
